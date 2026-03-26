362dac653e74574b94a485bf2bbd014ab2f646f1
KarMa2880 <marckarm2880@gmail.com>
2026-03-26 20:48:06 +0100

    tabele do danych wizyty_czesci oraz wizyty_uslugi

--- data/insert_uslugi.sql
+++ data/insert_uslugi.sql
@@ -1,13 +1,12 @@
-
-INSERT INTO Uslugi (nazwa, cena)
-VALUES
-    ('Wymiana oleju', 150.00),
-    ('Wymiana klocków hamulcowych', 300.00),
-    ('Diagnostyka komputerowa', 120.00),
-    ('Przegląd okresowy', 250.00),
-    ('Wymiana opon', 200.00),
-    ('Wymiana rozrządu', 500.00),
-    ('Wymiana zawieszenia', 400.00),
-    ('Serwis skrzyni biegów', 600.00),
-    ('Wymiana alternatora', 270.00),
-    ('Serwis klimatyzacji', 400.00);
+INSERT INTO `uslugi` 
+VALUES 
+	(1,'Wymiana oleju',150.00),
+    (2,'Wymiana klocków hamulcowych',300.00),
+    (3,'Diagnostyka komputerowa',120.00),
+    (4,'Przegląd okresowy',250.00),
+    (5,'Wymiana opon',200.00),
+    (6,'Wymiana rozrządu',500.00),
+    (7,'Wymiana zawieszenia',400.00),
+    (8,'Serwis skrzyni biegów',600.00),
+    (9,'Wymiana alternatora',270.00),
+    (10,'Serwis klimatyzacji',400.00);
