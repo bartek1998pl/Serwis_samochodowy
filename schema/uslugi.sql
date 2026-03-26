1f91a20eebc3f650f478ee411c44c2d3b36ba699
KarMa2880 <marckarm2880@gmail.com>
2026-03-27 00:29:34 +0100

    modyfikacja tabeli uslugi, dodanie danych do wizyty_czesci, wizyty_uslugi

--- schema/uslugi.sql
+++ schema/uslugi.sql
@@ -1,6 +1,6 @@
-
-CREATE TABLE Uslugi (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    nazwa VARCHAR(100),
-    cena DECIMAL(10,2)
-);
+CREATE TABLE `uslugi` (
+  `id_uslugi` int NOT NULL AUTO_INCREMENT,
+  `nazwa` varchar(100) DEFAULT NULL,
+  `cena` decimal(10,2) DEFAULT NULL,
+  PRIMARY KEY (`id_uslugi`)
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
