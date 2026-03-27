0951a8242e02e4edb45cd51134f15b929dddc3b0
bartek1998pl <bartek1998pl@gmail.com>
2026-03-27 21:35:59 +0100

    Create insert_wizyty.sql

---
+++ data/insert_wizyty.sql
@@ -0,0 +1,12 @@
+INSERT INTO `wizyty` 
+VALUES 
+		(1,1,'2026-01-10','Wymiana oleju i filtrów'),
+        (2,2,'2026-01-15','Wymiana klocków hamulcowych przód'),
+        (3,3,'2026-02-01','Diagnostyka komputerowa – kontrolka silnika'),
+        (4,4,'2026-02-12','Przegląd okresowy'),
+        (5,5,'2026-03-05','Wymiana opon na letnie'),
+        (6,6,'2026-03-20','Wymiana rozrządu'),
+        (7,7,'2026-03-02','Naprawa zawieszenia – amortyzatory'),
+        (8,8,'2026-03-18','Serwis skrzyni biegów'),
+        (9,9,'2026-02-03','Wymiana alternatora'),
+        (10,10,'2026-02-20','Serwis klimatyzacji – odgrzybianie i nabicie');
